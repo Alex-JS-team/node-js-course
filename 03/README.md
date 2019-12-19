d1406cdbe4cc238143c2b6f9837df107a1ebf9fa
Alex Ordynski <dr.hikari@gmail.com>
2019-12-19 20:57:00 +0200

    Add 03 lesson

---
+++ 03/README.md
@@ -0,0 +1,9 @@
+# 03
+
+Async JS
+Presentation: https://docs.google.com/presentation/d/11gGzzTkdZMVpBpcKxFRzuibVtG_jSf-rV06zRmd5RKU/edit?usp=sharing
+
+## Home work
+Weather APP.
+1. Get the temperature by coords.
+2. Get the city object by name string.
---
+++ practice/03/weather-app/index.js
@@ -0,0 +1,7 @@
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/bbb930df7abf0835186e2c34000cfd81/37.8267,-122.4233'
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+})
