01bbc210dfefaafdf6fefc16a2f4a9365221ee17
xxx44552 <amfABxPU>
2019-12-21 13:19:53 +0200

    test

--- practice/03/weather-app/index.js
+++ practice/03/weather-app/index.js
@@ -5,3 +5,5 @@ const url = 'https://api.darksky.net/forecast/bbb930df7abf0835186e2c34000cfd81/3
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
+
+console.log(1)
