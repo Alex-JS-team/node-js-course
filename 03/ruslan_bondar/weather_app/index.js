e3a71aaacba12016cb9151ccc9852952946fed7f
Ruslan Bondar <mailofbondik@gmail.com>
2019-12-21 13:54:57 +0200

    code fix

--- 03/ruslan_bondar/weather_app/index.js
+++ 03/ruslan_bondar/weather_app/index.js
@@ -1,34 +1,17 @@
-const request = require('request');
 const chalk = require('chalk');
-var term = require( 'terminal-kit' ).terminal;
+const utils = require('./utils');
+const term = require( 'terminal-kit' ).terminal;
 
 const queryLocation = 'Vinnytsia';
-const weatherToken = 'c5b4a80d47ef8ccf58c9cdbdd2f39f4c';
-const locationToken = 'pk.eyJ1IjoiYm9uZGlrIiwiYSI6ImNrNGQyem42NzB0aWszbW14bDFmZzI5MngifQ._MESARDPYIA7vKywYjD4Lw';
-const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${queryLocation}.json?access_token=${locationToken}`;
 
-request({ url: locationUrl, json: true }, (error, response) => {
-    const coordinates = [response.body.features[0].center[1], response.body.features[0].center[0]];
-
-    if (error) {
-        console.log('Error', error);
-    } else {
-        console.log('Coordinates:', coordinates);
-    }
-
-    const weatherUrl = `https://api.darksky.net/forecast/${weatherToken}/${coordinates[0]},${coordinates[1]}?units=si`;
-
-    request({ url: weatherUrl, json: true }, (error, response) => {
-        const weatherSummary = response.body.daily.data[0].summary;
-        const maxTemperature = response.body.daily.data[0].temperatureHigh;
-        const windSpeed = response.body.daily.data[0].windSpeed;
-
-        if (error) {
-            console.log('Error', error);
-        } else {
-            console.log(`The weather in ${chalk.green(queryLocation)} is ${chalk.blue(weatherSummary)}`);
-            console.log(`It is currently ${chalk.blue(maxTemperature)}`);
-            console.log(`Wind speed is ${chalk.blue(windSpeed)}`);
-        }
-    });
+utils.getLocation(queryLocation, (error, data) => {
+	if (error) {
+		console.log(chalk.red(error));
+	} else {
+		utils.getWeather(data, (error, data) => {
+			console.log(`The weather in ${chalk.green(queryLocation)} is ${chalk.blue(data.summary)}`);
+            console.log(`It is currently ${chalk.blue(data.temperature)}`);
+            console.log(`Wind speed is ${chalk.blue(data.windSpeed)}`);
+		});
+	}
 });
