19c583d5a034c4bed5044745d675e1d4892db5d1
Ruslan Bondar <mailofbondik@gmail.com>
2019-12-22 20:30:15 +0200

    +argv

--- 03/ruslan_bondar/weather_app/index.js
+++ 03/ruslan_bondar/weather_app/index.js
@@ -2,16 +2,16 @@ const chalk = require('chalk');
 const utils = require('./utils');
 const term = require( 'terminal-kit' ).terminal;
 
-const queryLocation = 'Vinnytsia';
+const searchQuery = process.argv.slice(2);
 
-utils.getLocation(queryLocation, (error, data) => {
+utils.getLocation(searchQuery, (error, data) => {
 	if (error) {
 		console.log(chalk.red(error));
 	} else {
 		utils.getWeather(data, (error, data) => {
-			console.log(`The weather in ${chalk.green(queryLocation)} is ${chalk.blue(data.summary)}`);
+			console.log(`The weather in ${chalk.green(searchQuery)} is ${chalk.blue(data.summary)}`);
             console.log(`It is currently ${chalk.blue(data.temperature)}`);
-            console.log(`Wind speed is ${chalk.blue(data.windSpeed)}`);
+			console.log(`Wind speed is ${chalk.blue(data.windSpeed)}`);
 		});
 	}
 });
