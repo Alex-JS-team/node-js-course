9ef5903ee10717ec8e61f9a975c959c0c2698c7a
Ruslan Bondar <mailofbondik@gmail.com>
2019-12-24 11:32:55 +0200

    terminal-kit usage

--- 03/ruslan_bondar/weather_app/index.js
+++ 03/ruslan_bondar/weather_app/index.js
@@ -4,14 +4,33 @@ const term = require('terminal-kit').terminal;
 
 const searchQuery = process.argv.slice(2);
 
-utils.getLocation(searchQuery, (error, data) => {
-	if (error) {
-		console.log(chalk.red(error));
+let progress = 0;
+let progressBar = term.progressBar({
+	width: 50,
+	title: 'Loading',
+	eta: true,
+	percent: true
+});
+
+const doProgress = () => {
+	progress += Math.random() / 10;
+	progressBar.update(progress);
+	
+	if (progress < 1) {
+		setTimeout(doProgress, 50 + Math.random() * 50);
 	} else {
-		utils.getWeather(data, (error, data) => {
-			console.log(`The weather in ${chalk.green(searchQuery)} is ${chalk.blue(data.summary)}`);
-            console.log(`It is currently ${chalk.blue(data.temperature)}`);
-			console.log(`Wind speed is ${chalk.blue(data.windSpeed)}`);
+		utils.getLocation(searchQuery, (error, data) => {
+			term('\n');
+			if (error) {
+				console.log(chalk.red(error));
+			} else {
+				utils.getWeather(data, (error, data) => {
+					console.log(`The weather in ${chalk.green(searchQuery)} is ${chalk.blue(data.summary)}`);
+					console.log(`It is currently ${chalk.blue(Math.ceil(data.temperature))}${chalk.blue(`℃`)}`);
+					console.log(`Wind speed is ${chalk.blue(data.windSpeed)} m/s`);
+				});
+			}
 		});
 	}
-});
+};
+doProgress();
