aba62397d64bd9bb2cbff1f74fba6de8b53a90c2
Artur Kudaev <artur.kudaev@ria.com>
2019-12-24 14:49:45 +0200

    hw_04_artur_kudaev

--- 04/artur_kudaev/index.js
+++ 04/artur_kudaev/index.js
@@ -2,31 +2,43 @@ const func = require('./func');
 const chalk = require('chalk');
 const term = require( 'terminal-kit' ).terminal ;
 
+const mycoords = [37.8267,-122.4233];
 
-var fs = require( 'fs' ) ;
+function showData(item, callback) {
+  func.getData(mycoords, (error, response) => {
+    if(error) {
+      console.log(chalk.red(error));
+    }else {
+      if(item === 'time') {
+        console.log(chalk.black.bgWhite.bold(new Date(response[item])));
+        console.log()
+        callback()
+      }else {
+        console.log(chalk.black.bgGreen.bold(`${item}: ${response[item]}`));
+        callback()
+      }
+    }
+  });
+}
 
-term.cyan( 'Choose a file:\n' ) ;
+term.cyan( 'Метеоданные Алькатрасе.\n' ) ;
 
-var items = fs.readdirSync( process.cwd() ) ;
+var items = [
+  'summary',
+  'time',
+  'temperature',
+  'ozone',
+] ;
 
-term.gridMenu( items , function( error , response ) {
+term.singleColumnMenu( items , function( error , response ) {
   term( '\n' ).eraseLineAfter.green(
       "#%s selected: %s (%s,%s)\n" ,
       response.selectedIndex ,
       response.selectedText ,
       response.x ,
       response.y
-  ) ;
-  process.exit() ;
+  );
+  if(response.selectedText) {
+    showData(`${response.selectedText}`, process.exit)
+  }
 } ) ;
-
-// const mycoords = [37.8267,-122.4233];
-//
-// func.getData(mycoords, (error, response) => {
-//   if(error) {
-//     console.log(chalk.red(error));
-//   }else {
-//     console.log(response)
-//     console.log(chalk.black.bgGreen.bold(response.ozone));
-//   }
-// });
