4ea8f7d1e9cb0a13a4d558507f3d5dfaa5d63bb0
Artur Kudaev <artur.kudaev@ria.com>
2019-12-26 17:33:27 +0200

    home work 05 - update

--- 04/artur_kudaev/2/index.js
+++ 04/artur_kudaev/2/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config.js');
+const data = require('./data');
 
 const app = express();
 
+var err = '';
+
 app.use(express.static(path.join(__dirname, '/public')));
 
+function error404(req, res, next) {
+  err = `https://${req.hostname+req.url}`;
+  console.log(`Error page - ${err} || ${new Date().toLocaleString()}`);
+  next();
+}
+
 app.get('/api', (req, res) => {
-  res.send({
-    data: 1,
-    text: 0
-  })
-})
+  res.json(data)
+});
 
 app.get('/', (req, res) => {
   res.sendFile('index.html' , { root : __dirname+'/public'});
+});
+app.use(error404);
 
+app.use(function(req, res) {
+  res.status(404).send(`<h1>404</h1><h2>${err?err:null}</h2>`);
 });
 
 app.listen(config.port, ()=>console.log('Start on port 3000..'));
