7b1eb12936f41e056e1280cf9e5535ab8112bd12
xxx44552 <amfABxPU>
2020-01-04 13:53:15 +0200

    hbs

--- 04/artur_kudaev/2/index.js
+++ 04/artur_kudaev/2/index.js
@@ -2,8 +2,59 @@ const express = require('express');
 const path = require('path');
 const config = require('./config.js');
 const data = require('./data');
+const hbs = require("hbs");
+const utils = require('./utils');
+var bodyParser = require('body-parser')
+
 
 const app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use('/hbs', (req, res) => {
+  res.render('index.hbs')
+})
+
+// app.get('/weather', ((req, res) => {
+//   const cityName = req.query.city;
+//   if(!cityName) {
+//     res.sendStatus(400)
+//   } else {
+//     utils.getWeatherByString(cityName, (error, weather) => {
+//       if (error) {
+//         res.sendStatus(500);
+//       } else {
+//         res.status(200).render('weather.hbs', {
+//           city: weather
+//         })
+//       }
+//     });
+//   }
+//
+// }));
+
+app.post('/getweather', urlencodedParser, ((req, res) => {
+  console.log(req.body);
+  // if(!cityName) {
+  //   res.sendStatus(400)
+  // } else {
+  //   utils.getWeatherByString(cityName, (error, weather) => {
+  //     if (error) {
+  //       res.sendStatus(500);
+  //     } else {
+  //       res.status(200).json(weather)
+  //     }
+  //   });
+  // }
+
+}));
+
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 var err = '';
 
@@ -22,6 +73,7 @@ app.get('/api', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile('index.html' , { root : __dirname+'/public'});
 });
+
 app.use(error404);
 
 app.use(function(req, res) {
