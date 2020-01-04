802e44e03416153a0e6e1cf85ee76144fe9e2ab9
Ruslan Bondar <mailofbondik@gmail.com>
2020-01-04 10:32:37 +0200

    starting handlebars

--- 05/ruslan_bondar/app.js
+++ 05/ruslan_bondar/app.js
@@ -1,4 +1,17 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('express-handlebars');
+const routes = require('./routes/index');
 
 const app = express();
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/', routes);
+
+app.listen(3000, () => {
+    console.log('Listening port 3000...');
+});
