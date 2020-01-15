3f9c2e8f8a72b5a5b19ae6edfc3d4ffa364bdb88
Ruslan Bondar <mailofbondik@gmail.com>
2020-01-16 00:47:37 +0200

    CRUD operations

--- 08/ruslan_bondar/task_manager/app.js
+++ 08/ruslan_bondar/task_manager/app.js
@@ -1,4 +1,5 @@
-const { MongoClient, ObjectID } = require('mongodb');
+const MongoClient = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -10,26 +11,41 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
 	const db = client.db(databaseName);
 
-	db.collection('users').insertOne({ name: 'Andrew', age: 27 })
-		.then(result => db.collection('users').deleteOne({ _id: ObjectID }))
+	db.collection('users').insertOne({ name: 'Billy', age: 27 });
 
+	db.collection('users').deleteOne({ _id: ObjectID });
 
+	db.collection('users').insertMany(
+		[
+			{ 
+				name: 'John', 
+				age: 29 
+			},
+			{ 
+				name: 'Bill', 
+				age: 25 
+			}
+		]
+	);
 
+	db.collection('users').deleteMany({ name: "Andrew" });
 
+	db.collection('users').updateOne(
+		{ 
+			_id: ObjectID("5e1f8ecfc60e703818897c64") 
+		}, 
+		{ 
+			$set: { 
+				name: "Bob" 
+			}
+		}
+	);
 
-	
+	db.collection('users').updateMany({ age: 27 }, { $set: { name: "Ann" }});
 
-	// db.collection('users').find({
-	// 	name: 'Andrew'
-	// }).toArray((error, result) => {
-	// 	console.log(result);
-	// });
-
-	// db.collection('users').deleteOne({
-	// 	name: 'Andrew'
-	// }, (error, result) => {
-	// 	if (error) {
-	// 		return console.log('Unable to delete user');
-	// 	}
-	// });
+	db.collection('users').find({
+		name: 'Andrew'
+	}).toArray((error, result) => {
+		console.log(result);
+	});
 });
