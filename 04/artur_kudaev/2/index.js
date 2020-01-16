d278216553d0a062289816e5c119305926a72298
Artur Kudaev <artur.kudaev@ria.com>
2020-01-16 17:16:43 +0200

    home work 07 - Mongoose

--- 04/artur_kudaev/2/index.js
+++ 04/artur_kudaev/2/index.js
@@ -2,12 +2,35 @@ const express = require('express');
 const path = require('path');
 const config = require('./config.js');
 const data = require('./data');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const hbs = require("hbs");
 
 const app = express();
 
+
+
+const jsonParser = bodyParser.json();
+
+//Увеличиваю позволенный размер файла
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+mongoose.connect("mongodb://localhost:27017/users", { useNewUrlParser: true });
+
+const userScheme = new Schema({
+  name: String,
+  age: Number
+});
+const User = mongoose.model("User", userScheme);
+
 var err = '';
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public'), {index: false}));
 
 function error404(req, res, next) {
   err = `https://${req.hostname+req.url}`;
@@ -20,8 +43,36 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html' , { root : __dirname+'/public'});
+  User.find({}, function (error, response) {
+    if(error) throw error;
+    res.render('index.hbs', {
+      arr: response
+    })
+  })
 });
+
+app.get('/form', (req, res) => {
+  res.sendFile('form.html' , { root : __dirname+'/public'});
+});
+
+app.post('/save', jsonParser, function(req, res){
+  if(!req.body) return res.sendStatus(400);
+  const user = new User({
+    name: req.body.userName,
+    age: req.body.age
+  });
+  user.save().then(user=>console.log(`Save: ${user}`))
+  res.sendStatus(200)
+});
+
+app.post('/del', jsonParser, function(req, res){
+  if(!req.body) return res.sendStatus(400);
+  const id = req.body.id;
+  User.deleteOne({_id: id}).then(del => console.log(del) )
+  console.log(id)
+  res.sendStatus(200)
+});
+
 app.use(error404);
 
 app.use(function(req, res) {
@@ -29,3 +80,4 @@ app.use(function(req, res) {
 });
 
 app.listen(config.port, ()=>console.log('Start on port 3000..'));
+
