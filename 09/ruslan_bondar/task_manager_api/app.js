dc183001076e383b3d56636b2378644cd16de2d1
Ruslan Bondar <mailofbondik@gmail.com>
2020-01-16 14:20:19 +0200

    CRUD operations

--- 09/ruslan_bondar/task_manager_api/app.js
+++ 09/ruslan_bondar/task_manager_api/app.js
@@ -19,8 +19,54 @@ const me = new User({
     age: 26
 });
 
+const notMe = new User({
+    name: 'Unknown',
+    age: 100
+});
+
 me.save().then(() => {
     console.log(me);
+}).catch((error) => {
+    console.log(error);
+})
+
+notMe.save().then(() => {
+    console.log(notMe);
+}).catch((error) => {
+    console.log(error);
+})
+
+User.find().then((users) => {
+    console.log(users);
+}).catch((error) => {
+    console.log(error);
+})
+
+User.insertMany(
+    [
+        { name: 'Bob', age: 30 },
+        { name: 'Bill', age: 40 }
+    ]
+).then((users) => {
+    console.log(users);
+}).catch((error) => {
+    console.log(error);
+})
+
+User.deleteMany({ age: 26 }).then((users) => {
+    console.log(users);
+}).catch((error) => {
+    console.log(error);
+})
+
+User.deleteOne({ name: 'Unknown' }).then((user) => {
+    console.log(user);
+}).catch((error) => {
+    console.log(error);
+})
+
+User.update({ age: 30 }, { name: 'Billie' }).then((user) => {
+    console.log(user);
 }).catch((error) => {
     console.log(error);
 })
